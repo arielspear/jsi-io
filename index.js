81fb972e5338578fed561faa31c300c3576f605b
Grant Stampfli <mail@gstampfli.com>
2014-05-19 14:20:24 -0700

    made index list words in file order

--- index.js
+++ index.js
@@ -2,12 +2,17 @@
 module.exports.words = {};
 
 var fs = require('fs');
-var optnions = {encoding: 'utf8' };
+var options = { encoding: 'utf8' };
 
-fs.readFile('./file1.txt', options, function(err, read){
-
-});
-
+var list1 = fs.readFile('./file1.txt', options, function(err, read){
+console.log(read); 
+fs.readFile('./file2.txt', options, function(err, read){
+console.log(read);
+})});
+// var list2 = fs.readFile('./file2.txt', options, function(err, read){
+// console.log(read);
+// });
+// list1(list2)
 
 // fs.readFile('./path/to/file', { encoding: 'utf8' }, function(err, contents) {
 //   console.log(contents);

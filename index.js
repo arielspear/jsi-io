69a09fc08e7b1d26894697dbb0bad080b6dd45f2
Ariel Spear <simpleredfox@gmail.com>
2014-05-19 13:49:07 -0700

    program options, two files with words and index has fs stuff

---
+++ index.js
@@ -0,0 +1,14 @@
+
+module.exports.words = {};
+
+var fs = require('fs');
+var optnions = {encoding: 'utf8' };
+
+fs.readFile('./file1.txt', options, function(err, read){
+
+});
+
+
+// fs.readFile('./path/to/file', { encoding: 'utf8' }, function(err, contents) {
+//   console.log(contents);
+// });

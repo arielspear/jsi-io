2139b72a389ce7966eac32ca94723276c2bf4fd5
Ariel Spear <simpleredfox@gmail.com>
2014-05-19 14:50:10 -0700

    makes array split on newlines and gives those arrays

--- index.js
+++ index.js
@@ -4,16 +4,28 @@ module.exports.words = {};
 var fs = require('fs');
 var options = { encoding: 'utf8' };
 
-var list1 = fs.readFile('./file1.txt', options, function(err, read){
-console.log(read); 
-fs.readFile('./file2.txt', options, function(err, read){
-console.log(read);
-})});
-// var list2 = fs.readFile('./file2.txt', options, function(err, read){
-// console.log(read);
-// });
-// list1(list2)
+var each = function(array, fn) {
+  var continueIterating = function(array, n, fn) {
+    if (n < array.length) {
+      fn(array[n]);
+      continueIterating(array, n+1, fn);
+    }
+  };
+  continueIterating(array, 0, fn);
+};
 
-// fs.readFile('./path/to/file', { encoding: 'utf8' }, function(err, contents) {
-//   console.log(contents);
-// });
+
+fs.readFile('./file1.txt', options, function(err, read){
+	// console.log(read); 
+	array1 = (read.split("\n"));
+	console.log(array1);
+	fs.readFile('./file2.txt', options, function(err, read){
+		// console.log(read);
+		array2 = (read.split("\n"));
+		console.log(array2);
+	});
+});
+
+
+// array1 = (list1.split("\n"));
+// console.log(array1);
